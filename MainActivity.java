d6e4ac91c9d40ae1bf5159fd3f294c7278b45650
TranPhuHung98 <tranphuhung98@gmail.com>
2018-05-08 00:16:18 +0700

    demo lifecycle

---
+++ MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.user.demolifecyclereal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this,"onCreate()",Toast.LENGTH_SHORT);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(MainActivity.this,"onStart()",Toast.LENGTH_SHORT);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(MainActivity.this,"onStop()",Toast.LENGTH_SHORT);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(MainActivity.this,"onDestroy()",Toast.LENGTH_SHORT);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(MainActivity.this,"onPause()",Toast.LENGTH_SHORT);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(MainActivity.this,"onResume()",Toast.LENGTH_SHORT);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(MainActivity.this,"onRestart()",Toast.LENGTH_SHORT);
+    }
+}
